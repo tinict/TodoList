2dde83d77a6624f20ec833a122c4cb2a055a2705
Tín Nguyễn <89054327+tinict@users.noreply.github.com>
2022-09-29 11:06:55 +0700

    test-02

--- public/index.js
+++ public/index.js
@@ -1,19 +1,16 @@
 console.log("Test");
-ElementIpWork = document.querySelector('.addTask');
-ElementAddWork = document.querySelector('.btn-addTask');
-ElementList = document.querySelector('.list');
+const ElementIpWork = document.querySelector('.addTask');
+const ElementAddWork = document.querySelector('.btn-addTask');
+const ElementList = document.querySelector('.list');
 
-const Tasks = [];
+var Tasks = [];
 
-ElementAddWork.onclick = () => {
-    console.log(ElementIpWork.value);
-    Tasks.push(ElementIpWork.value);
-    console.log(Tasks);
+const RenderTasks = () => {
     const htmlTasks = Tasks.map((task) => {
         return (
             `<div class="item-list">
                 <div class="item-left">
-                    <input class = "radio-item" type="radio">
+                    <input class = "radio-item" type="radio" name= "task-item">
                     <span class="work-item">${task}</span>
                 </div>
                 <div class="item-right">
@@ -21,6 +18,33 @@ ElementAddWork.onclick = () => {
                 </div>
             </div>`
         );
-    })
+    });
     ElementList.innerHTML = htmlTasks.join('');
 };
+
+const RemoveTask = (str) => {
+    console.log('Delete Successful');
+    Tasks = Tasks.filter((Task) => {
+        return Task !== str;
+    })
+    RenderTasks();
+    const ElementCheckTasks = document.querySelectorAll('.radio-item');
+    const ElementWorkItem = document.querySelectorAll('.work-item');
+    console.log(ElementCheckTasks);
+    for (let i = 0; i < ElementCheckTasks.length; ++i) {
+        ElementCheckTasks[i].addEventListener('click', () => {
+            console.log('Successful');
+            console.log(ElementWorkItem[i].textContent);
+            RemoveTask(ElementWorkItem[i].textContent);
+        });
+    };
+}
+
+ElementAddWork.onclick = () => {
+    Tasks.push(ElementIpWork.value);
+    RenderTasks();
+    RemoveTask();
+};
+
+
+
